3f2c39899ab9c2e83514ffd1d88335c6274fd6e7
dbonbon <yrfriedman@gmail.com>
2023-05-04 16:47:28 +0200

    initial

---
+++ pipit/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-%p5iqlms)+h$%qnx+^edb*z(y_1wc8iik(8%cy!+o#axt%nb5c"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
